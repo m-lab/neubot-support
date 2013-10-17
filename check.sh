809df436c169c648d88fd754de06443220191893
Simone Basso <bassosimone@gmail.com>
2013-10-17 23:19:28 +0200

    add check.sh: initial script to check the deployment result

---
+++ check.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Public domain, 2013 Simone Basso <bassosimone@gmail.com>
+
+#
+# Make sure that the nodes are correctly deployed
+#
+
+DEBUG=
+
+for NODE in $*; do
+    (
+        set -e
+        echo "=== BEGIN CHECK $NODE ==="
+
+        cd neubot
+
+        ./bin/neubot dash -A $NODE -f
+
+        echo "=== CHECK DEPLOY $NODE ==="
+        echo ""
+        echo ""
+    )
+
+    sleep 3
+done
